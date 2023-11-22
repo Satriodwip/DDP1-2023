3182923f6ee007fc8d7eca7c02b05855923fb207
Satriodwip <146051211+Satriodwip@users.noreply.github.com>
2023-11-22 14:36:08 +0700

    Add files via upload

---
+++ DDP-Python/Tugas_9.py
@@ -0,0 +1,30 @@
+def profil(nama, alamat, gender, umur, hobi):
+    print("nama:", nama)
+    print("alamat:", alamat)
+    print("gender:", gender)
+    print("umur:", umur)
+    print("hobi:", hobi)
+profil ("Satrio Dwi Pujianto", "Kp Jatijajar RT/08 RW/08", "Male", "20", "Touring")
+    
+
+def indikatorkelulusan(nilai):
+    if nilai <= 60:
+        print("Gagal")
+    elif nilai >= 61 and nilai <= 70:
+        print("Baik")
+    elif nilai >= 71 and nilai <= 80:
+        print("Sangat Baik")
+    elif nilai >= 81 and nilai <= 100:
+        print("Istimewa")
+nilai = int(input("masukan nilai: "))
+indikatorkelulusan(nilai)
+
+def sebut_ganjil(angka):
+    for a in range(angka):
+        if a % 2 != 0 :
+            print(a,end=' ')
+bilangan = int(input("masukan angka: "))
+sebut_ganjil(bilangan)
+
+        
+    
